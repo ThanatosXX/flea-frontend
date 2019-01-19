818d2cc5eb2212131e856055e4c61548529cad6d
geebos <2823970399@qq.com>
2019-01-19 19:13:34 +0800

    信息页面

--- pages/messgage/messgage.js
+++ pages/messgage/messgage.js
@@ -1,66 +1,32 @@
-// pages/messgage/messgage.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    data: {
+        actions: [
+                {
+                width: 100,
+                name: '删除',
+                background: '#ed3f14',
+                color: '#fff',
+            },
+            {
+                name: '取消',
+                width: 100,
+                color: '#80848f',
+                fontsize: '20',
+            }
+        ],
+        message_list: [1, 2, 3, 4, 5, 6, 7],
+    },
 
-  },
+    onLoad: function(options) {
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    },
 
-  },
+    change: function(event){
+        console.log(event, '222222');
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    click: function(event){
+        console.log(event);
+    },
 })
