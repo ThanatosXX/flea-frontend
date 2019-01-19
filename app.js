ae619200c6f5a5dff4ec5fe48c3dabf1084b40ca
louyuanyang <louyuanyang@outlook.com>
2019-01-19 14:34:07 +0800

    "我的"页面更新
    "我的"页面更新

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ App({
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
   onLaunch: function () {
+    // wx.getSetting({
+    //   success: function(res){
+    //     wx.setStorageSync("authorize", res.authSetting)
+    //   }
+    // })
     
   },
 
--- pages/my/my.js
+++ pages/my/my.js
@@ -5,14 +5,57 @@ Page({
    * 页面的初始数据
    */
   data: {
+    "userInfo":{
 
+    },
+    "showAuthorizeButton":true
   },
 
+  goto:function(e){
+    wx.navigateTo({
+      url: e.currentTarget.dataset.href + "/" + e.currentTarget.dataset.href,
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let that = this;
+    // 授权验证
+    wx.getSetting({
+      success:function(res){
+        if (res.authSetting['scope.userInfo']){
+          // 如果已授权则关闭按钮
+          that.setData({
+            "showAuthorizeButton": false
+          })
+          // 并获取头像
+          wx.getUserInfo({
+            success: function (res) {
+              that.setData({
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
 
+  onGotUserInfo(e) {
+    let that = this;
+    if (e.detail.userInfo){
+      this.setData({
+        "showAuthorizeButton":false
+      })
+      wx.getUserInfo({
+        success: function (res) {
+          that.setData({
+            userInfo: res.userInfo
+          })
+        }
+      })
+    }
   },
 
   /**
