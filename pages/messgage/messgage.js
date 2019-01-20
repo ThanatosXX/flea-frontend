4a726233f5e260fb7c2300e0aa4306165a2f4a94
louyuanyang <louyuanyang@outlook.com>
2019-01-20 20:56:25 +0800

    滑动菜单项更新
    按QQ的

--- pages/messgage/messgage.js
+++ pages/messgage/messgage.js
@@ -1,20 +1,28 @@
 Page({
 
     data: {
-        actions: [
-                {
-                width: 100,
-                name: '删除',
-                background: '#ed3f14',
-                color: '#fff',
-            },
-            {
-                name: '取消',
-                width: 100,
-                color: '#80848f',
-                fontsize: '20',
-            }
-        ],
+      actions: [
+        {
+          name: '置顶',
+          width: 100,
+          color: '#fff',
+          fontsize: '20',
+          background: "#bbb",
+        },
+        {
+          name: '标为未读',
+          width: 100,
+          color: '#fff',
+          background: "#E6A23C",
+          fontsize: '20',
+        },
+        {
+          width: 100,
+          name: '删除',
+          background: '#ed3f14',
+          color: '#fff',
+        }
+      ],
         message_list: [1, 2, 3, 4, 5, 6, 7],
     },
 
